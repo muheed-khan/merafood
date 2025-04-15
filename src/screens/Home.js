0bc0fd0b965b961c6c569f63f35cb5d8d382d848
Muheed Ahmed Khan <muheedk999@gmail.com>
2025-04-15 21:15:16 +0500

    Initialization

---
+++ src/screens/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+export default function () {
+  return (
+    <div>
+
+        <div>
+                <Navbar/>
+                <div>Body</div>
+                <Footer/>
+        </div>
+
+    </div>
+  )
+}
