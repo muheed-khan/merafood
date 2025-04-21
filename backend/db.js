dbf01639e72a9fb842e40c9566d1466c0fb82bae
Muheed Ahmed Khan <muheedk999@gmail.com>
2025-04-22 03:22:04 +0500

    back-end connection established

--- backend/db.js
+++ backend/db.js
@@ -1,33 +1,34 @@
-// const mongoose = require('mongoose');
-// const URI = "mongodb+srv://muheedk999:121826@cluster0.wbnvmu4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-// const mongoDB = async()=>{
-//     try {
-//         await mongoose.connect(URI);
-//         console.log("✅ Connected to MongoDB");
-//       } catch (error) {
-//         console.error("❌ Failed to connect to MongoDB:", error);
-//       }
-// }
-// // merafood123
+const mongoose = require('mongoose');
+const URI = "mongodb://localhost:27017/"
+const mongoDB = async()=>{
+    try {
+        await mongoose.connect(URI);
+        console.log("✅ Connected to MongoDB");
+      } catch (error) {
+        console.error("❌ Failed to connect to MongoDB:", error);
+      }
+}
+// merafood123
 // module.exports = mongoDB;
+mongoDB()
 
 
 
 
-const mongoose = require("mongoose");
-const connectionString =
-  "mongodb+srv://muheedk999:merafood123@cluster0.wbnvmu4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const mongoose = require("mongoose");
+// const connectionString =
+//   "mongodb+srv://muheedk999:merafood123@cluster0.wbnvmu4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+// mongoose
+//   .connect(connectionString, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to MongoDB:", error);
+//   });
 
-module.exports = mongoose.co
+// module.exports = mongoose.co
